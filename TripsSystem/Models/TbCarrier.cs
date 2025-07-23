734ff4951180c7db2daf67c7f5bbdf7d652515f8
AmmarBawazirTech <AmmarBawazirTech@gmail.com>
2025-07-23 17:22:07 +0300

    Complete the task

---
+++ TripsSystem/Models/TbCarrier.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shipment.Models;
+
+public partial class TbCarrier
+{
+    public Guid Id { get; set; }
+
+    public string CarrierName { get; set; } = null!;
+
+    public Guid? UpdatedBy { get; set; }
+
+    public int CurrentState { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+}
---
+++ TripsSystem/Models/TbUserSebder.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shipment.Models;
+
+public partial class TbUserSebder
+{
+    public Guid Id { get; set; }
+
+    public Guid UserId { get; set; }
+
+    public string SenderName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Phone { get; set; } = null!;
+
+    public Guid CityId { get; set; }
+
+    public string Address { get; set; } = null!;
+
+    public Guid? UpdatedBy { get; set; }
+
+    public int CurrentState { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public Guid CreatedBy { get; set; }
+
+    public DateTime? UpdatedDate { get; set; }
+}
